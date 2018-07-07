0331833e0885c1df3771a3e7c8de8d030f96dd71
user <user@arch.lan>
2018-07-07 21:38:53 +0100

    more terminal stuff

--- state_game.lua
+++ state_game.lua
@@ -1,20 +1,16 @@
 state_game = {}
 
 function state_game:enter()
+	terminal = require("terminal")
+	terminal:init()
+end
+
+function state_game:update()
+	terminal:update()
 end
 
 function state_game:draw()
-	
-	--[[for y = 0, h/fonth-3 do
-		local str = ""
-		for x = 1, w/fontw do
-			str = str.."a"
-		end
-
-		love.graphics.print(str, 2, y*fonth)
-	end]]
-
-	love.graphics.print(">>", 5, h-fonth*1.75)
+	terminal:draw()
 end
 
 return state_game
--- terminal.lua
+++ terminal.lua
@@ -1,6 +1,33 @@
 local terminal = {}
 
 function terminal:init()
+	terminal.lines = {
+	}
+
+	terminal.input = ""
+	terminal.input_active = true
+end
+
+function terminal:prompt()
+end
+
+function terminal:update()
+end
+
+function terminal:draw()
+	local iv = #terminal.lines-2
+	if (iv > h/fonth-3) then
+		iv = h/fonth-3
+	end
+
+	for i = 0, iv do
+		local y = h-i*fonth-fonth*3
+		love.graphics.print(terminal.lines[#terminal.lines-i], 2, y)
+	end
+
+	if (terminal.input_active) then
+		love.graphics.print(">>", 5, h-fonth*1.75)
+	end
 end
 
 return terminal
